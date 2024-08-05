006014d62f0dc1ee21cac52c688db64508e9ac9f
Zengry10 <93141837+Zengry10@users.noreply.github.com>
2024-08-05 10:52:47 +0200

    feat: add component Etoile to design system

---
+++ design-system/HomePage.vue
@@ -0,0 +1,23 @@
+<!-- En Nuxt, l'auto importation est disponible, il n'est donc pas nécessaire d'importer les composants dans chaque fichier. Il suffit de les utiliser directement. -->
+
+
+<script setup>
+
+</script>
+
+<template>
+
+    <UtilitaireEtoile
+        :icon="'star'"
+        :iconSize="'16px'"
+        :iconColor="'#FFD700'"
+        :color="'#FFD700'"
+        :starCount="3"
+    />
+
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ design-system/components/Utilitaire/Etoile.vue
@@ -0,0 +1,62 @@
+<!-- Composant étoile pour afficher des étoiles -->
+
+<script setup>
+    import { defineProps } from 'vue'
+
+    const emit = defineEmits(['delete'])
+
+    const { isMobile } = useDevice()
+
+    const props = defineProps({
+    icon: {
+        type: String,
+        default: ''
+    },
+
+    iconSize: {
+        type: String,
+        default: '16px'
+    },
+
+    iconColor: {
+        type: String,
+        default: ''
+    },
+
+    color: {
+        type: String,
+        default: ''
+    },
+
+    starCount: {
+        type: Number,
+        default: 5
+    }
+})
+</script>
+
+<template>
+    <div class="container--image">
+        <Icon
+            v-for="index in starCount"
+            :key="index"
+            class="ui-p__icon"
+            :name="icon"
+            :size="iconSize"
+            :color="iconColor || color"
+        />
+    </div>
+</template>
+
+<style scoped>
+
+    .container--image {
+        display: flex;
+        justify-content: center;
+    }
+
+    .ui-p__icon {
+        padding: 3px 0;
+    }
+
+</style>
